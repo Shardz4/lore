6b4a01756ec05e0cbc15a919a1a3dce0922ec96c
Arnav Sharma <arnav4324@gmail.com>
2026-06-20 11:48:26 +0530

    Disable background OTLP tracer connection logs if endpoint is unreachable

--- agents/analyst-agent/src/telemetry.rs
+++ agents/analyst-agent/src/telemetry.rs
@@ -1,7 +1,35 @@
 use opentelemetry_sdk::trace::Tracer;
 use opentelemetry_otlp::WithExportConfig;
+use std::net::ToSocketAddrs;
+use std::net::TcpStream;
+use std::time::Duration;
+
+pub fn is_reachable(endpoint: &str) -> bool {
+    let mut cleaned = endpoint;
+    if let Some(pos) = endpoint.find("://") {
+        cleaned = &endpoint[pos + 3..];
+    }
+    if let Some(pos) = cleaned.find('/') {
+        cleaned = &cleaned[..pos];
+    }
+
+    let addrs = match cleaned.to_socket_addrs() {
+        Ok(mut a) => a.next(),
+        Err(_) => return false,
+    };
+
+    if let Some(addr) = addrs {
+        TcpStream::connect_timeout(&addr, Duration::from_millis(300)).is_ok()
+    } else {
+        false
+    }
+}
 
 pub fn init_tracer(endpoint: &str) -> Result<Tracer, Box<dyn std::error::Error>> {
+    if !is_reachable(endpoint) {
+        return Err("OTLP collector endpoint is unreachable".into());
+    }
+
     let exporter = opentelemetry_otlp::new_exporter()
         .tonic()
         .with_endpoint(endpoint);
@@ -19,3 +47,4 @@ pub fn init_tracer(endpoint: &str) -> Result<Tracer, Box<dyn std::error::Error>>
 
     Ok(tracer)
 }
+
